f840f858afcb778875184088bea5d9c9bad9aa74
nevarsystems <66962243+nevarsystems@users.noreply.github.com>
2022-01-19 14:25:25 +0530

    Add files via upload

--- assets/mbr-flip-card/mbr-flip-card.js
+++ assets/mbr-flip-card/mbr-flip-card.js
@@ -1,2 +1,15 @@
+function bendBottomCorner() {
+    $(".flip-card:not(.builderCard)").each(function() {
+        $(this).addClass('builderCard');
+    });
+}
 
-function bendBottomCorner(){$(".flip-card:not(.builderCard)").each(function(){$(this).addClass("builderCard")})}var isBuilder=$("html").hasClass("is-builder");if(isBuilder)$(document).on("add.cards",function(){0!=$(".flip-card").length&&bendBottomCorner()});
+//Mobirise initialization
+var isBuilder = $('html').hasClass('is-builder'); 
+if (isBuilder) {
+    $(document).on('add.cards', function() {
+        if ($('.flip-card').length != 0) {
+            bendBottomCorner();
+        }
+    });
+}
